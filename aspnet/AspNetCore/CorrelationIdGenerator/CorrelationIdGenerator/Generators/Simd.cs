217807521acb1bda1173429af107f39776617f05
Günther Foidl <gue@korporal.at>
2019-05-21 16:05:42 +0200

    Extracted printer
    Cf. https://gist.github.com/gfoidl/37ba3471eaeb4f9b7fd7bd5662e8d11e

--- aspnet/AspNetCore/CorrelationIdGenerator/CorrelationIdGenerator/Generators/Simd.cs
+++ aspnet/AspNetCore/CorrelationIdGenerator/CorrelationIdGenerator/Generators/Simd.cs
@@ -2,19 +2,17 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Runtime.Intrinsics;
 using System.Runtime.Intrinsics.X86;
 using System.Threading;
+using static gfoidl.Tools.Intrinsics.Printer;
 
 namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Infrastructure
 {
     internal static class CorrelationIdGenerator5
     {
-        public static bool EnablePrint { get; set; } = true;
-
         // Base32 encoding - in ascii sort order for easy text based sorting
         private static readonly char[] s_encode32Chars = "0123456789ABCDEFGHIJKLMNOPQRSTUV".ToCharArray();
 
@@ -236,93 +234,6 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Infrastructure
 
             short s = c1.AsInt16().GetElement(4);   // 4 = (Vector128<sbyte>.Count - sizeof(long)) / sizeof(short)
             Unsafe.WriteUnaligned(ref b, s);
-        }
-        //---------------------------------------------------------------------
-        [DebuggerStepThrough, Conditional("DEBUG")]
-        private static void PrintHeader()
-        {
-            if (!EnablePrint) return;
-
-            Console.Write("{0,-10}: ", "byte-idx");
-
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            for (int i = Vector128<sbyte>.Count - 1; i >= 0; --i)
-                Console.Write("  {0,2} ", i);
-            Console.WriteLine();
-            Console.ResetColor();
-        }
-        //---------------------------------------------------------------------
-        [DebuggerStepThrough, Conditional("DEBUG")]
-        private static void Print(long value, string caption = null)
-        {
-            if (!EnablePrint) return;
-
-            if (!string.IsNullOrWhiteSpace(caption))
-                Console.Write("{0,-10}: ", caption);
-
-            ref sbyte val = ref Unsafe.As<long, sbyte>(ref value);
-
-            for (int i = 0; i < sizeof(long); ++i)
-                Console.Write("{0}", new string(' ', 2 + 2 + 1));
-
-            for (int i = sizeof(long) - 1; i >= 0; --i)
-            {
-                sbyte tmp = Unsafe.Add(ref val, i);
-
-                Console.ForegroundColor = tmp != 0
-                    ? ConsoleColor.Green
-                    : ConsoleColor.DarkGray;
-
-                Console.Write("0x{0:x2} ", tmp);
-            }
-
-            Console.ResetColor();
-            Console.WriteLine();
-        }
-        //---------------------------------------------------------------------
-        [DebuggerStepThrough, Conditional("DEBUG")]
-        private static void Print(Vector128<sbyte> vec, string caption = null, bool printAsScii = false, bool insertEmptyLineBefore = false, bool insertEmpyLineAfter = false)
-        {
-            if (!EnablePrint) return;
-
-            if (insertEmptyLineBefore)
-                Console.WriteLine();
-
-            if (!string.IsNullOrWhiteSpace(caption))
-                Console.Write("{0,-10}: ", caption);
-
-            if (printAsScii)
-                PrintAsAscii();
-            else
-                PrintAsHex();
-
-            Console.ResetColor();
-            Console.WriteLine();
-
-            if (insertEmpyLineAfter)
-                Console.WriteLine();
-            //-----------------------------------------------------------------
-            void PrintAsHex()
-            {
-                for (int i = Vector128<sbyte>.Count - 1; i >= 0; --i)
-                {
-                    sbyte value = vec.GetElement(i);
-
-                    Console.ForegroundColor = value != 0
-                        ? ConsoleColor.Green
-                        : ConsoleColor.DarkGray;
-
-                    Console.Write("0x{0:x2} ", value);
-                }
-            }
-            //-----------------------------------------------------------------
-            void PrintAsAscii()
-            {
-                Console.ForegroundColor = ConsoleColor.Cyan;
-
-                for (int i = Vector128<sbyte>.Count - 1; i >= 0; --i)
-                    Console.Write("{0,4} ", (char)vec.GetElement(i));
-            }
         }
     }
 }
