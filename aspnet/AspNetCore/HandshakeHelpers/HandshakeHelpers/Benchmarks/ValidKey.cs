16332847a70a0234a7c6d2454ca0382a90d29f4d
Günther Foidl <gue@korporal.at>
2019-08-16 21:14:55 +0200

    HandshakeHelpers benchmarks

---
+++ aspnet/AspNetCore/HandshakeHelpers/HandshakeHelpers/Benchmarks/ValidKey.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace HandshakeHelpers.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class ValidKey
+    {
+        private string _key = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+        //---------------------------------------------------------------------
+        [Benchmark(Baseline = true)]
+        public bool Baseline() => Helpers.Baseline.IsRequestKeyValid(_key);
+        //---------------------------------------------------------------------
+        [Benchmark]
+        public bool PR12386() => Helpers.PR12386.IsRequestKeyValid(_key);
+        //---------------------------------------------------------------------
+        [Benchmark]
+        public bool Vectorized() => Helpers.Vectorized.IsRequestKeyValid(_key);
+    }
+}
---
+++ aspnet/AspNetCore/HandshakeHelpers/HandshakeHelpers/Helpers/Baseline.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HandshakeHelpers.Helpers
+{
+    public static class Baseline
+    {
+        public static bool IsRequestKeyValid(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return false;
+            }
+            try
+            {
+                byte[] data = Convert.FromBase64String(value);
+                return data.Length == 16;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
