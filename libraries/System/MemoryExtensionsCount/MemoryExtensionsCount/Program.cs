cfa9f3dba175815aa8db481cc449a98e1bba2606
Günther Foidl <gue@korporal.at>
2023-02-26 18:10:59 +0100

    Remainder vectorized

--- libraries/System/MemoryExtensionsCount/MemoryExtensionsCount/Program.cs
+++ libraries/System/MemoryExtensionsCount/MemoryExtensionsCount/Program.cs
@@ -38,8 +38,8 @@ using BenchmarkDotNet.Attributes;
 BenchmarkDotNet.Running.BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).RunAll();
 #endif
 
-[ShortRunJob]
-[DisassemblyDiagnoser]
+//[ShortRunJob]
+//[DisassemblyDiagnoser]
 [GenericTypeArguments(typeof(byte))]
 [GenericTypeArguments(typeof(short))]
 [GenericTypeArguments(typeof(int))]
@@ -49,7 +49,7 @@ public class Bench<T> where T : struct, INumberBase<T>
     private T[]? _source;
     private T _value;
 
-    public int Length { get; set; } = 100;
+    public int Length { get; set; } = 2 * Vector256<T>.Count - 1;
 
     [GlobalSetup]
     public void Setup()
@@ -58,6 +58,16 @@ public class Bench<T> where T : struct, INumberBase<T>
         _value = T.One;
 
         _source[0] = _value;
+
+        if (this.Length > Vector128<T>.Count)
+        {
+            _source[^Vector128<T>.Count] = _value;
+
+            if (this.Length > Vector256<T>.Count)
+            {
+                _source[^Vector256<T>.Count] = _value;
+            }
+        }
         _source[^1] = _value;
     }
 
@@ -135,48 +145,63 @@ public static class SpanHelpers
     public static int CountValueType_PR<T>(ref T current, T value, int length) where T : struct, IEquatable<T>?
     {
         int count = 0;
-
         ref T end = ref Unsafe.Add(ref current, length);
-        if (Vector128.IsHardwareAccelerated && length >= Vector128<T>.Count)
-        {
-            if (Vector256.IsHardwareAccelerated && length >= Vector256<T>.Count)
-            {
-                Vector256<T> targetVector = Vector256.Create(value);
-                ref T oneVectorAwayFromEndMinus1 = ref Unsafe.Subtract(ref end, Vector256<T>.Count - 1);
-                do
-                {
-                    count += BitOperations.PopCount(Vector256.Equals(Vector256.LoadUnsafe(ref current), targetVector).ExtractMostSignificantBits());
-                    current = ref Unsafe.Add(ref current, Vector256<T>.Count);
-                }
-                while (Unsafe.IsAddressLessThan(ref current, ref oneVectorAwayFromEndMinus1));
 
-                if (Unsafe.IsAddressLessThan(ref current, ref Unsafe.Subtract(ref end, Vector128<T>.Count - 1)))
-                {
-                    count += BitOperations.PopCount(Vector128.Equals(Vector128.LoadUnsafe(ref current), Vector128.Create(value)).ExtractMostSignificantBits());
-                    current = ref Unsafe.Add(ref current, Vector128<T>.Count);
-                }
-            }
-            else
+        if (Vector256.IsHardwareAccelerated && length >= Vector256<T>.Count)
+        {
+            Vector256<T> targetVector = Vector256.Create(value);
+            ref T oneVectorAwayFromEnd = ref Unsafe.Subtract(ref end, Vector256<T>.Count);
+            do
             {
-                Vector128<T> targetVector = Vector128.Create(value);
-                ref T oneVectorAwayFromEndMinus1 = ref Unsafe.Subtract(ref end, Vector128<T>.Count - 1);
-                do
-                {
-                    count += BitOperations.PopCount(Vector128.Equals(Vector128.LoadUnsafe(ref current), targetVector).ExtractMostSignificantBits());
-                    current = ref Unsafe.Add(ref current, Vector128<T>.Count);
-                }
-                while (Unsafe.IsAddressLessThan(ref current, ref oneVectorAwayFromEndMinus1));
+                count += BitOperations.PopCount(Vector256.Equals(Vector256.LoadUnsafe(ref current), targetVector).ExtractMostSignificantBits());
+                current = ref Unsafe.Add(ref current, Vector256<T>.Count);
+            }
+            while (!Unsafe.IsAddressGreaterThan(ref current, ref oneVectorAwayFromEnd));
+
+            nint remaining = Unsafe.ByteOffset(ref current, ref end) / Unsafe.SizeOf<T>();
+            if (remaining > 0)
+            {
+                uint mask = Vector256.Equals(Vector256.LoadUnsafe(ref oneVectorAwayFromEnd), targetVector).ExtractMostSignificantBits();
+
+                // The mask contains some elements that may be double-checked, so shift them away in order to get the correct pop-count.
+                nint overlaps = Vector256<T>.Count - remaining;
+                mask >>= (int)overlaps;
+                count += BitOperations.PopCount(mask);
             }
         }
-
-        while (Unsafe.IsAddressLessThan(ref current, ref end))
+        else if (Vector128.IsHardwareAccelerated && length >= Vector128<T>.Count)
         {
-            if (current.Equals(value))
+            Vector128<T> targetVector = Vector128.Create(value);
+            ref T oneVectorAwayFromEnd = ref Unsafe.Subtract(ref end, Vector128<T>.Count);
+            do
             {
-                count++;
+                count += BitOperations.PopCount(Vector128.Equals(Vector128.LoadUnsafe(ref current), targetVector).ExtractMostSignificantBits());
+                current = ref Unsafe.Add(ref current, Vector128<T>.Count);
             }
+            while (!Unsafe.IsAddressGreaterThan(ref current, ref oneVectorAwayFromEnd));
 
-            current = ref Unsafe.Add(ref current, 1);
+            nint remaining = Unsafe.ByteOffset(ref current, ref end) / Unsafe.SizeOf<T>();
+            if (remaining > 0)
+            {
+                uint mask = Vector128.Equals(Vector128.LoadUnsafe(ref oneVectorAwayFromEnd), targetVector).ExtractMostSignificantBits();
+
+                // The mask contains some elements that may be double-checked, so shift them away in order to get the correct pop-count.
+                nint overlaps = Vector128<T>.Count - remaining;
+                mask >>= (int)overlaps;
+                count += BitOperations.PopCount(mask);
+            }
+        }
+        else
+        {
+            while (Unsafe.IsAddressLessThan(ref current, ref end))
+            {
+                if (current.Equals(value))
+                {
+                    count++;
+                }
+
+                current = ref Unsafe.Add(ref current, 1);
+            }
         }
 
         return count;
