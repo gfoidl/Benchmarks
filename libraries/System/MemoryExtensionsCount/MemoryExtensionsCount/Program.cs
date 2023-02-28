a4906f0bd1acea1616bf4160de6179ebdc7089e4
Günther Foidl <gue@korporal.at>
2023-02-28 12:25:34 +0100

    Updated implementation

--- libraries/System/MemoryExtensionsCount/MemoryExtensionsCount/Program.cs
+++ libraries/System/MemoryExtensionsCount/MemoryExtensionsCount/Program.cs
@@ -3,6 +3,7 @@ using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
 using BenchmarkDotNet.Attributes;
 
 {
@@ -42,24 +43,32 @@ using BenchmarkDotNet.Attributes;
 BenchmarkDotNet.Running.BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).RunAll();
 #endif
 
-[ShortRunJob]
-[DisassemblyDiagnoser]
-//[GenericTypeArguments(typeof(byte))]
-//[GenericTypeArguments(typeof(short))]
+//[ShortRunJob]
+//[DisassemblyDiagnoser]
+[GenericTypeArguments(typeof(byte))]
+[GenericTypeArguments(typeof(short))]
 [GenericTypeArguments(typeof(int))]
-//[GenericTypeArguments(typeof(long))]
+[GenericTypeArguments(typeof(long))]
 public class Bench<T> where T : struct, INumberBase<T>, IMinMaxValue<T>
 {
     private T[]? _source;
     private T _value;
 
-    //[ParamsSource(nameof(ValuesForLength))]
-    public int Length { get; set; } = 2 * Vector256<T>.Count - 1;
+    [ParamsSource(nameof(ValuesForLength))]
+    public int Length { get; set; } = 10_000;
+
+    //[Params(false, true)]
+    public bool AllValuesSet { get; set; } = true;
 
     public static IEnumerable<int> ValuesForLength()
     {
-        yield return 2 * Vector256<T>.Count - 1;
-        yield return Vector256<T>.Count + 1;
+        //yield return 2 * Vector256<T>.Count - 1;
+        //yield return Vector256<T>.Count + 1;
+
+        yield return Vector128<T>.Count;
+        yield return Vector256<T>.Count;
+        yield return 100;
+        yield return 1_000;
     }
 
     [GlobalSetup]
@@ -68,18 +77,25 @@ public class Bench<T> where T : struct, INumberBase<T>, IMinMaxValue<T>
         _source = new T[this.Length];
         _value = T.One;
 
-        _source[0] = _value;
-
-        if (this.Length > Vector128<T>.Count)
+        if (this.AllValuesSet)
         {
-            _source[^Vector128<T>.Count] = _value;
-
-            if (this.Length > Vector256<T>.Count)
-            {
-                _source[^Vector256<T>.Count] = _value;
-            }
+            _source.AsSpan().Fill(T.One);
         }
-        _source[^1] = _value;
+        else
+        {
+            _source[0] = _value;
+
+            if (this.Length > Vector128<T>.Count)
+            {
+                _source[^Vector128<T>.Count] = _value;
+
+                if (this.Length > Vector256<T>.Count)
+                {
+                    _source[^Vector256<T>.Count] = _value;
+                }
+            }
+            _source[^1] = _value;
+        }
     }
 
     //[Benchmark(Baseline = true)]
@@ -91,7 +107,7 @@ public class Bench<T> where T : struct, INumberBase<T>, IMinMaxValue<T>
         return SpanHelpers.CountValueType(ref source, _value, this.Length);
     }
 
-    //[Benchmark(Baseline = true)]
+    [Benchmark(Baseline = true)]
     public int PR()
     {
         Debug.Assert(_source is not null);
@@ -250,7 +266,7 @@ public static class SpanHelpers
             {
                 Vector256<T> targetVector = Vector256.Create(value);
                 Vector256<T> accumulator = Vector256<T>.Zero;
-                T i = T.Zero;
+                int i = 0;
                 ref T oneVectorAwayFromEnd = ref Unsafe.Subtract(ref end, Vector256<T>.Count);
 
                 do
@@ -261,11 +277,13 @@ public static class SpanHelpers
                     i++;
                     current = ref Unsafe.Add(ref current, Vector256<T>.Count);
 
-                    if (i == T.MaxValue)
+                    // To avoid overflow in the accumulator for sbyte, short.
+                    if ((typeof(T) == typeof(sbyte) && i == (byte)(sbyte)(object)T.MaxValue)
+                     || (typeof(T) == typeof(short) && i == (ushort)(short)(object)T.MaxValue))
                     {
                         count += SumVector256(accumulator);
                         accumulator = Vector256<T>.Zero;
-                        i = T.Zero;
+                        i = 0;
                     }
                 }
                 while (!Unsafe.IsAddressGreaterThan(ref current, ref oneVectorAwayFromEnd));
@@ -289,7 +307,7 @@ public static class SpanHelpers
             {
                 Vector128<T> targetVector = Vector128.Create(value);
                 Vector128<T> accumulator = Vector128<T>.Zero;
-                T i = T.Zero;
+                int i = 0;
                 ref T oneVectorAwayFromEnd = ref Unsafe.Subtract(ref end, Vector128<T>.Count);
 
                 do
@@ -300,11 +318,13 @@ public static class SpanHelpers
                     i++;
                     current = ref Unsafe.Add(ref current, Vector128<T>.Count);
 
-                    if (i == T.MaxValue)
+                    // To avoid overflow in the accumulator for sbyte, short.
+                    if ((typeof(T) == typeof(sbyte) && i == (byte)(sbyte)(object)T.MaxValue)
+                     || (typeof(T) == typeof(short) && i == (ushort)(short)(object)T.MaxValue))
                     {
                         count += SumVector128(accumulator);
                         accumulator = Vector128<T>.Zero;
-                        i = T.Zero;
+                        i = 0;
                     }
                 }
                 while (!Unsafe.IsAddressGreaterThan(ref current, ref oneVectorAwayFromEnd));
@@ -324,60 +344,6 @@ public static class SpanHelpers
                     return count;
                 }
             }
-
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            static int SumVector256(Vector256<T> counter)
-            {
-                T sum = Vector256.Sum(counter);
-
-                if (typeof(T) == typeof(sbyte))
-                {
-                    return (sbyte)(object)sum;
-                }
-                else if (typeof(T) == typeof(short))
-                {
-                    return (short)(object)sum;
-                }
-                else if (typeof(T) == typeof(int))
-                {
-                    return (int)(object)sum;
-                }
-                else if (typeof(T) == typeof(long))
-                {
-                    return (int)(long)(object)sum;
-                }
-                else
-                {
-                    throw new NotSupportedException();
-                }
-            }
-
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            static int SumVector128(Vector128<T> counter)
-            {
-                T sum = Vector128.Sum(counter);
-
-                if (typeof(T) == typeof(sbyte))
-                {
-                    return (sbyte)(object)sum;
-                }
-                else if (typeof(T) == typeof(short))
-                {
-                    return (short)(object)sum;
-                }
-                else if (typeof(T) == typeof(int))
-                {
-                    return (int)(object)sum;
-                }
-                else if (typeof(T) == typeof(long))
-                {
-                    return (int)(long)(object)sum;
-                }
-                else
-                {
-                    throw new NotSupportedException();
-                }
-            }
         }
 
         while (Unsafe.IsAddressLessThan(ref current, ref end))
@@ -391,5 +357,143 @@ public static class SpanHelpers
         }
 
         return count;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static int SumVector256(Vector256<T> accumulator)
+        {
+            if (typeof(T) == typeof(sbyte))
+            {
+                if (Avx2.IsSupported)
+                {
+                    Vector256<ushort> sad0 = Avx2.SumAbsoluteDifferences(accumulator.AsByte(), Vector256<byte>.Zero);
+                    Vector256<ushort> sad1 = Avx2.Shuffle(sad0.AsInt32(), 0x2).AsUInt16();
+                    Vector256<ushort> tmp = sad0 + sad1;
+                    Vector128<ushort> t1 = tmp.GetUpper();
+
+                    return tmp.ToScalar() + t1.ToScalar();
+                }
+                else
+                {
+                    int sum = 0;
+
+                    for (int i = 0; i < Vector256<sbyte>.Count; ++i)
+                    {
+                        sum += (byte)(sbyte)(object)accumulator[i];
+                    }
+
+                    return sum;
+                }
+            }
+            else if (typeof(T) == typeof(short))
+            {
+                if (Avx2.IsSupported)
+                {
+                    Vector256<short> vec = accumulator.AsInt16();
+                    Vector256<int> hadd = Avx2.MultiplyAddAdjacent(vec, Vector256<short>.One);
+                    Vector256<int> hadd1 = Avx2.HorizontalAdd(hadd, hadd);
+                    Vector256<int> hadd2 = Avx2.HorizontalAdd(hadd1, hadd1);
+                    Vector128<int> t1 = hadd2.GetUpper();
+
+                    return hadd2.ToScalar() + t1.ToScalar();
+                }
+                else
+                {
+                    int sum = 0;
+
+                    for (int i = 0; i < Vector256<short>.Count; ++i)
+                    {
+                        sum += (ushort)(short)(object)accumulator[i];
+                    }
+
+                    return sum;
+                }
+            }
+            else if (typeof(T) == typeof(int))
+            {
+                return (int)(object)Vector256.Sum(accumulator);
+            }
+            else if (typeof(T) == typeof(long))
+            {
+                if (Avx2.IsSupported)
+                {
+                    Vector256<long> v0 = accumulator.AsInt64();
+                    Vector256<long> v1 = Avx2.Permute4x64(v0, 0b_10_11_00_01);
+                    Vector256<long> s0 = v0 + v1;
+                    Vector256<long> s1 = Avx2.Permute4x64(s0, 0x2);
+                    Vector256<long> sum = s0 + s1;
+
+                    return (int)sum.ToScalar();
+                }
+
+                return (int)(long)(object)Vector256.Sum(accumulator);
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static int SumVector128(Vector128<T> accumulator)
+        {
+            if (typeof(T) == typeof(sbyte))
+            {
+                if (Sse2.IsSupported)
+                {
+                    Vector128<ushort> sad0 = Sse2.SumAbsoluteDifferences(accumulator.AsByte(), Vector128<byte>.Zero);
+                    Vector128<ushort> sad1 = Sse2.Shuffle(sad0.AsInt32(), 0x2).AsUInt16();
+                    return sad0.ToScalar() + sad1.ToScalar();
+                }
+                else
+                {
+                    int sum = 0;
+
+                    for (int i = 0; i < Vector128<sbyte>.Count; ++i)
+                    {
+                        sum += (byte)(sbyte)(object)accumulator[i];
+                    }
+
+                    return sum;
+                }
+            }
+            else if (typeof(T) == typeof(short))
+            {
+                if (Ssse3.IsSupported)
+                {
+                    Vector128<short> vec = accumulator.AsInt16();
+                    Vector128<int> hadd = Sse2.MultiplyAddAdjacent(vec, Vector128<short>.One);
+                    Vector128<int> hadd1 = Ssse3.HorizontalAdd(hadd, hadd);
+                    Vector128<int> hadd2 = Sse2.Shuffle(hadd1, 0x1);
+                    Vector128<int> sum = hadd1 + hadd2;
+
+                    return sum.ToScalar();
+                }
+                else
+                {
+                    int sum = 0;
+
+                    for (int i = 0; i < Vector128<short>.Count; ++i)
+                    {
+                        sum += (ushort)(short)(object)accumulator[i];
+                    }
+
+                    return sum;
+                }
+            }
+            else if (typeof(T) == typeof(int))
+            {
+                return (int)(object)Vector128.Sum(accumulator);
+            }
+            else if (typeof(T) == typeof(long))
+            {
+                long l0 = accumulator.AsInt64().ToScalar();
+                long l1 = accumulator.AsInt64().GetElement(1);
+                return (int)(l0 + l1);
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+        }
     }
 }
